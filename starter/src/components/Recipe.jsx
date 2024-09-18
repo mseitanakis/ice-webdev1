baedb8d838c324fe5e5e18e191939250b7b586f5
Cole Nelson <ctnelson1997@gmail.com>
2024-09-18 10:51:20 -0500

    f24 wd1

---
+++ starter/src/components/Recipe.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import { Button, Card } from "react-bootstrap";
+
+export default function Recipe(props) {
+    return <div>
+        <p>I am a recipe!</p>
+    </div>
+}
