9ee050ce68048256c6bc27af9d426019bc87864f
mseitanakis <mseitanakis@gmail.com>
2024-09-25 20:18:19 -0500

    finished lecture

--- starter/src/components/AllRecipes.jsx
+++ starter/src/components/AllRecipes.jsx
@@ -14,8 +14,29 @@ const RECIPE_PASTA = JSON.parse(`{"name":"Classic Spaghetti Carbonara","author":
 const RECIPE_CHILI = JSON.parse(`{"name":"7-Ingredient Chili","author":"OpenAI","img":{"location":"https://raw.githubusercontent.com/CS571-F24/ice-api-static-content/main/chili.png","description":"A delicious bowl of chili!"},"keywords":["hearty","flavorful","simple","comforting"],"reviews":[{"txt":"A burst of warmth and flavor in every spoonful; simple yet irresistible!","rating":5},{"txt":"The perfect blend of spice and comfort, an easy go-to chili recipe.","rating":4},{"txt":"Loved the hearty texture and rich taste - a new family favorite!","rating":5},{"txt":"Quick, flavorful, and satisfying - this chili hits all the right notes!","rating":5}],"ingredients":{"ground beef":{"amount":1,"unit":"lb"},"kidney beans":{"amount":15,"unit":"oz","misc":"drained and rinsed"},"diced tomatoes":{"amount":14.5,"unit":"oz","misc":"with juice"},"chili powder":{"amount":2,"unit":"tablespoon"},"onion":{"amount":1,"misc":"diced"},"bell pepper":{"amount":1,"misc":"diced"},"ground cumin":{"amount":1,"unit":"teaspoon"}},"recipe":["Cook Meat with Vegetables: In a large pot, cook the ground beef, diced onion, and diced bell pepper over medium heat until the meat is no longer pink and the vegetables are softened.","Add Remaining Ingredients: To the pot, add the kidney beans, diced tomatoes (with their juice), chili powder, ground cumin, and salt to taste. If the chili is too thick, add a little water to reach your desired consistency.","Simmer: Bring the mixture to a boil, then reduce the heat and simmer for about 20-30 minutes to allow the flavors to meld. Stir occasionally.","Serve: Enjoy your chili as is, or with toppings like shredded cheese or sour cream."]}`);
 
 export default function AllRecipes(props) {
+    
+    const [chili, setChili] = useState();
+
+    useEffect(() => {
+        fetch("https://cs571api.cs.wisc.edu/rest/f24/ice/chili", {
+            headers: {
+                "X-CS571-ID": CS571.getBadgerId()
+            }
+
+        })
+        .then(res => {
+            console.log(res.status);
+            return res.json()
+        })
+        .then(data => {
+            console.log(data);
+            setChili(data);
+        })
+    }, [])
+    
     return <div>
         <h1>Welcome to Badger Recipes!</h1>
-        <Recipe/>
+        {chili ? <Recipe {...chili}/> : <p>Still Loading...</p>}
+        
     </div>
 }
--- starter/src/components/Recipe.jsx
+++ starter/src/components/Recipe.jsx
@@ -2,7 +2,19 @@ import { useState } from "react";
 import { Button, Card } from "react-bootstrap";
 
 export default function Recipe(props) {
-    return <div>
-        <p>I am a recipe!</p>
-    </div>
+    
+    const [likes, setLikes] = useState(0)
+
+    return <Card style={{marginTop: "0.5rem", maxWidth: "20rem", margin: "auto"}}>
+                      
+        <h2>{props.name}</h2>
+        <p>{props.author}</p>
+        <p>{props.keywords.join(", ")}</p>
+        <p>{likes} likes</p>
+        <Button variant="secondary" onClick={() => {
+            setLikes(likes => likes + 1)
+            setLikes(likes => likes + 1)
+        }}>Like</Button>
+        
+    </Card>
 }
